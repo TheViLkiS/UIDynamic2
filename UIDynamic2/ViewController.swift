5ff48c5ee20f8c5080e5607d02ff6d53c54cf726
ViLkiS <thevilkis@gmail.com>
2023-03-06 13:32:02 +0400

    привязка к пальцу

--- UIDynamic2/ViewController.swift
+++ UIDynamic2/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createSmallSquareView()
+        createGestureRecognizer()
+        createAnchorView()
+        createAnimationAndBehaviors()
     }
 
     
@@ -42,5 +45,33 @@ class ViewController: UIViewController {
         view.addSubview(anchorView)
     }
     
+    // создадим регистратор жеста паранормирования
+    func createGestureRecognizer() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handletPan(paramPan: )))
+        
+        view.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    // Создаем столкновение и прикрепление
+    func createAnimationAndBehaviors() {
+        animator = UIDynamicAnimator(referenceView: view)
+        // столкновения
+        let collision = UICollisionBehavior(items: [squareView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        // прикрепления
+        attachmentBehavior = UIAttachmentBehavior(item: squareView, attachedToAnchor: anchorView.center)
+        animator.addBehavior(collision)
+        animator.addBehavior(attachmentBehavior!)
+    }
+    
+     // определяет где палец туда где красный квадрат а потом привязка к большому
+    @objc func handletPan(paramPan: UIPanGestureRecognizer) {
+        let tapPoint = paramPan.location(in: view)
+        print(tapPoint)
+        attachmentBehavior?.anchorPoint = tapPoint
+        anchorView.center = tapPoint
+    }
+    
 }
+
 
